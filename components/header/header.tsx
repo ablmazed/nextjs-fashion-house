9f6f221db308c4ff684b57139b733965fef5df66
mazed <ablmazed@gmail.com>
2025-05-27 00:31:54 +0600

    update 3

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,7 +1,39 @@
-export default function header() {
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
   return (
-    <div>
-      <h2>Header</h2>
-    </div>
+    <header>
+      <nav>
+        <div className="navbar justify-between bg-base-300">
+          <div>
+            <label htmlFor="my-drawer" className="btn btn-square btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                ></path>
+              </svg>
+            </label>
+            <Link href="/" className="btn btn-ghost text-lg">
+              Next Amazona V2
+            </Link>
+          </div>
+          Menu
+        </div>
+        <div className="bg-base-300 block md:hidden text-center pb-3">
+          SearchBox
+        </div>
+      </nav>
+    </header>
   )
 }
+
+export default Header
