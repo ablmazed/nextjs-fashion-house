293e7a919c5b6e46ff22ea3b6e9b4daa346ab734
mazed <ablmazed@gmail.com>
2025-05-27 15:43:31 +0600

    update 10

--- app/(front)/product/[slug]/page.tsx
+++ app/(front)/product/[slug]/page.tsx
@@ -1,12 +1,8 @@
 import data from '@/lib/data'
 import Link from 'next/link'
 
-interface PageProps {
-  params: { slug: string }
-}
-
-export default async function ProductDetails({ params }: PageProps) {
-  const product = data.products.find((x) => x.slug === params.slug)
+export default async function ProductDetails() {
+  const product = data.products
   if (!product) {
     return <div>Product not found</div>
   }
