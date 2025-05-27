719d1bd3d7adec65f7f7d3206800a00a0078e5ef
mazed <ablmazed@gmail.com>
2025-05-27 21:36:36 +0600

    update 18

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,5 +1,5 @@
 import ProductItem from '@/components/products/ProductItem'
-import products from '@/lib/data'
+import data from '@/lib/data'
 
 export default async function Home() {
   return (
@@ -7,7 +7,7 @@ export default async function Home() {
       <h2 className="text-2xl py-2">Latest Products Here</h2>
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-        {products.map((product) => (
+        {data.products.map((product) => (
           <ProductItem key={product.slug} product={product} />
         ))}
       </div>
--- app/(front)/product/[slug]/page.tsx
+++ app/(front)/product/[slug]/page.tsx
@@ -1,33 +1,67 @@
-import products from '@/lib/data'
 import Image from 'next/image'
-import React from 'react'
+import Link from 'next/link'
+import data from '@/lib/data'
 
-export default function ProductDetails({
-  productdata,
+export default async function ProductDetails({
+  params,
 }: {
-  productdata: { slug: string }
+  params: { slug: string }
 }) {
-  const slug = productdata?.slug
-  const product = products.find((p) => p.slug === slug)
-  if (!product) return <div>Product not found</div>
-
+  const product = data.products.find((x) => x.slug === params.slug)
+  if (!product) {
+    return <div>Product not found</div>
+  }
   return (
-    <div className="p-4">
-      <div className="grid md:grid-cols-2 gap-4">
-        <Image
-          src={product.image}
-          alt={product.name}
-          width={600}
-          height={600}
-          className="w-full object-cover rounded-lg"
-        />
+    <>
+      <div className="my-2">
+        <Link href="/">back to products</Link>
+      </div>
+      <div className="grid md:grid-cols-4 md:gap-3">
+        <div className="md:col-span-2">
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            sizes="100vw"
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+          ></Image>
+        </div>
         <div>
-          <h1 className="text-3xl font-bold mb-4">{product.name}</h1>
-          <p className="text-lg mb-2">Brand: {product.brand}</p>
-          <p className="text-xl text-primary mb-4">${product.price}</p>
-          <p>{product.description}</p>
+          <ul className="space-y-4">
+            <li>
+              <h1 className="text-xl">{product.name}</h1>
+            </li>
+
+            <li> {product.brand}</li>
+            <li>
+              <div className="divider"></div>
+            </li>
+            <li>
+              Description: <p>{product.description}</p>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div className="card  bg-base-300 shadow-xl mt-3 md:mt-0">
+            <div className="card-body">
+              <div className="mb-2 flex justify-between">
+                <div>Price</div>
+                <div>${product.price}</div>
+              </div>
+              <div className="mb-2 flex justify-between">
+                <div>Status</div>
+                <div>
+                  {product.countInStock > 0 ? 'In stock' : 'Unavailable'}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
