07996bda2143688b2655aa747f64dd7bab312761
mazed <ablmazed@gmail.com>
2025-06-01 20:50:25 +0600

    update 65

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
-import Header from '@/components/header/header'
 import './globals.css'
-import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Providers from '@/components/Providers'
+import Sidebar from '@/components/Sidebar'
+import DrawerButton from '@/components/DrawerButton'
+import Header from '@/components/header/header'
 
 const inter = Inter({ subsets: ['latin'] })
-
-export const metadata: Metadata = {
-  title: 'Fashion-House',
-  description: 'Fashion House Description',
-}
 
 export default function RootLayout({
   children,
@@ -20,12 +16,29 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-          <div className="min-h-screen flex flex-col">
-            <Header />
-            {children}
-            <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-              <p>Copyright © 2023 - All right reserved by Next Amazona V2</p>
-            </footer>
+          <div className="drawer">
+            <DrawerButton />
+            <div className="drawer-content">
+              {/* Page content here */}
+              <div className="min-h-screen flex flex-col">
+                <Header />
+                {children}
+                <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+                  <p>
+                    Copyright © 2023 - All right reserved by{' '}
+                    {process.env.NEXT_PUBLIC_APP_NAME || 'Next Shop'}
+                  </p>
+                </footer>
+              </div>
+            </div>
+            <div className="drawer-side">
+              <label
+                htmlFor="my-drawer"
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <Sidebar />
+            </div>
           </div>
         </Providers>
       </body>
