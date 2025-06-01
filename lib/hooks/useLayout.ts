22ee6f608eb346e3f613bc06e00f73d69847f955
mazed <ablmazed@gmail.com>
2025-06-01 19:56:16 +0600

    update 60

--- lib/hooks/useLayout.ts
+++ lib/hooks/useLayout.ts
@@ -1,28 +1,28 @@
-import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+// import { create } from 'zustand'
+// import { persist } from 'zustand/middleware'
 
-type Layout = {
-  theme: string
-}
-const initialState: Layout = {
-  theme: 'system',
-}
+// type Layout = {
+//   theme: string
+// }
+// const initialState: Layout = {
+//   theme: 'system',
+// }
 
-export const layoutStore = create<Layout>()(
-  persist(() => initialState, {
-    name: 'layoutStore',
-  })
-)
+// export const layoutStore = create<Layout>()(
+//   persist(() => initialState, {
+//     name: 'layoutStore',
+//   })
+// )
 
-export default function useLayoutService() {
-  const { theme } = layoutStore()
+// export default function useLayoutService() {
+//   const { theme } = layoutStore()
 
-  return {
-    theme,
-    toggleTheme: () => {
-      layoutStore.setState({
-        theme: theme === 'dark' ? 'light' : 'dark',
-      })
-    },
-  }
-}
+//   return {
+//     theme,
+//     toggleTheme: () => {
+//       layoutStore.setState({
+//         theme: theme === 'dark' ? 'light' : 'dark',
+//       })
+//     },
+//   }
+// }
