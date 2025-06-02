892fc1ba3b8453d9b26fd6aa4e636ad1b3e8cbc0
mazed <ablmazed@gmail.com>
2025-06-02 09:47:51 +0600

    update 74

--- app/api/admin/orders/route.ts
+++ app/api/admin/orders/route.ts
@@ -1,21 +1,22 @@
+import dbConnect from '@/lib/dbConnect'
+import { auth } from '@/lib/auth'
+import OrderModel from '@/lib/models/OrderModel'
 
-   import dbConnect from '@/lib/dbConnect'
-   import { auth } from '@/lib/auth'
-   import OrderModel from '@/lib/models/OrderModel'
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const GET = auth(async (req: any) => {
+  if (!req.auth) {
+    return Response.json(
+      { message: 'unauthorized' },
+      {
+        status: 401,
+      }
+    )
+  }
+  await dbConnect()
+  const orders = await OrderModel.find()
+    .sort({ createdAt: -1 })
+    .populate('user', 'name')
 
-   export const GET = auth(async (req: any) => {
-     if (!req.auth) {
-       return Response.json(
-         { message: 'unauthorized' },
-         {
-           status: 401,
-         }
-       )
-     }
-     await dbConnect()
-     const orders = await OrderModel.find()
-       .sort({ createdAt: -1 })
-       .populate('user', 'name')
-
-     return Response.json(orders)
-   }) as any
+  return Response.json(orders)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+}) as any
