230a6c08d699d46d80ef7a39a886a0beebb397bf
mazed <ablmazed@gmail.com>
2025-06-02 23:50:21 +0600

    update 93

--- app/api/admin/users/[id]/route.ts
+++ app/api/admin/users/[id]/route.ts
@@ -3,6 +3,78 @@ import dbConnect from '@/lib/dbConnect'
 import UserModel from '@/lib/models/UserModel'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const GET = auth(async (...args: any) => {
+  const [req, { params }] = args
+  if (!req.auth) {
+    return Response.json(
+      { message: 'unauthorized' },
+      {
+        status: 401,
+      }
+    )
+  }
+  await dbConnect()
+  const user = await UserModel.findById(params.id)
+  if (!user) {
+    return Response.json(
+      { message: 'user not found' },
+      {
+        status: 404,
+      }
+    )
+  }
+  return Response.json(user)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+}) as any
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const PUT = auth(async (...p: any) => {
+  const [req, { params }] = p
+  if (!req.auth) {
+    return Response.json(
+      { message: 'unauthorized' },
+      {
+        status: 401,
+      }
+    )
+  }
+
+  const { name, email, isAdmin } = await req.json()
+
+  try {
+    await dbConnect()
+
+    const user = await UserModel.findById(params.id)
+    if (user) {
+      user.name = name
+      user.email = email
+      user.isAdmin = Boolean(isAdmin)
+
+      const updatedUser = await user.save()
+      return Response.json({
+        message: 'User updated successfully',
+        user: updatedUser,
+      })
+    } else {
+      return Response.json(
+        { message: 'User not found' },
+        {
+          status: 404,
+        }
+      )
+    }
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    return Response.json(
+      { message: err.message },
+      {
+        status: 500,
+      }
+    )
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+}) as any
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const DELETE = auth(async (...args: any) => {
   const [req, { params }] = args
   if (!req.auth) {
